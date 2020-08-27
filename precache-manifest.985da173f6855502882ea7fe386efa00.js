c9de4a9a6edada3878370a68ec569a805081ff26
alexposm <alexposm@gmail.com>
2020-08-27 19:01:53 +0300

    Updates

---
+++ precache-manifest.985da173f6855502882ea7fe386efa00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abf4bc1f47fe5e7e3553318339c5aca",
+    "url": "/alexposm-card/index.html"
+  },
+  {
+    "revision": "ba494617dd9345de2b8c",
+    "url": "/alexposm-card/static/css/main.e12b261b.chunk.css"
+  },
+  {
+    "revision": "02891ccd2cd4fe43046e",
+    "url": "/alexposm-card/static/js/2.d33ffd53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alexposm-card/static/js/2.d33ffd53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba494617dd9345de2b8c",
+    "url": "/alexposm-card/static/js/main.c2679905.chunk.js"
+  },
+  {
+    "revision": "4b7f9e985ec134b6a96e",
+    "url": "/alexposm-card/static/js/runtime-main.c5afa433.js"
+  }
+]);
