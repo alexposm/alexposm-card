fd413b82df4759b1bc4252e979ca63c87615d3b2
alexposm <alexposm@gmail.com>
2020-08-27 18:31:23 +0300

    add dists

---
+++ build/precache-manifest.1fdb2ebc7fc1b542c61623bb3184bcc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3c0586c19fab913cec818fdedeb301",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0edf7acf7f847e7c9632",
+    "url": "./static/css/main.e12b261b.chunk.css"
+  },
+  {
+    "revision": "02891ccd2cd4fe43046e",
+    "url": "./static/js/2.d33ffd53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d33ffd53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edf7acf7f847e7c9632",
+    "url": "./static/js/main.23cc958f.chunk.js"
+  },
+  {
+    "revision": "11d8a9fd9419ad8efc95",
+    "url": "./static/js/runtime-main.169be587.js"
+  }
+]);
